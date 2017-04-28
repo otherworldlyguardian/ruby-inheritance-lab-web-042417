b891b8ee0d62eab514cfc9b65bb09740062817a6
otherworldlyguardian <otherworldlyguardian@gmail.com>
2017-04-28 15:32:39 -0400

    all the knowledge

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+require "user.rb"
+
+class Student < User
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(tidbit)
+    self.knowledge << tidbit
+  end
 
 end
